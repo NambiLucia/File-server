73647b07e6bd067591892396d4f06cd7746a726f
lucia nambi <nambilucia@gmail.com>
2024-07-28 16:45:37 +0300

    add protect node

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-import express from "express";
-import morgan from "morgan"
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-
-
-
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 const app = express();
+
+const PORT = 4000;
+const PASSWORD = 'admin123';
 
 
 
 
 app.use(morgan("dev"));
+app.use(express.urlencoded({extended:true}))// parse form data
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/node-course',(req,res,next)=>{//protect node
+    const password=req.query.password;
+    if(password===PASSWORD){
+        next();
+
+    }
+    else{
+        res.redirect('/login');
+    }
+})
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the Server")
@@ -22,9 +32,23 @@ app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,'public',"login.html"))
 
 })
+app.post("/login", (req,res)=>{ //handle login submission
+    const {password}= req.body
+    if(password===PASSWORD){
+        res.redirect(`/node-course?password=${PASSWORD}`)
+    }
+    else{
+        res.send("wrong password. Please try again")
+    }
+});
+
+app.get("/node-course",(req,res)=>{
+    res.sendFile(path.join(__dirname,'public',"node-course.html"))
+
+})
 
 
-const PORT=4000;
+
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}/`)
 
